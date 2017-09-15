c92a7118e7bb978c90cd60d4a36857d518da4c6e
Robert Aguirre <RobertAguirre@users.noreply.github.com>
2017-09-15 14:27:19 -0400

    users controller and basic view setup

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
 
   def index
     if user_signed_in?
-      redirect_to welcome_show_path
+      redirect_to users_show_path
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'users/show'
+
   root 'welcome#index'
 
   get 'welcome/show' => 'welcome#show'
