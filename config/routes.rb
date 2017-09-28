12c6c309757d8ad4a324c106fa935cb7968a8396
Robert Aguirre <RobertAguirre@users.noreply.github.com>
2017-09-28 16:18:39 -0400

    mark todo items as complete and basic styling

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
   resources :todo_lists do
-    resources :todo_items
+    resources :todo_items do
+      member do
+        patch :complete
+      end
+    end
   end
 
   # get 'users/show'
